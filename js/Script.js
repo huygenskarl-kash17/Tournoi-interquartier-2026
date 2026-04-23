0ed76d190366daa7c68a505367d696f6e6c944a0
=Huygens Karl <=24kk122si@udbl.ac.cd>
2026-04-24 00:25:41 +0200

    ss

--- js/Script.js
+++ js/Script.js
@@ -1,4 +1,33 @@
 
+/* MENU MOBILE */
+const nav = document.querySelector("nav ul");
+document.querySelector(".logo").addEventListener("click", () => {
+nav.classList.toggle("active");
+});
+
+/* SLIDER */
+let index = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(){
+slides.forEach(s => s.style.display = "none");
+index++;
+if(index > slides.length){index = 1;}
+slides[index-1].style.display = "block";
+setTimeout(showSlide, 3000);
+}
+
+if(slides.length > 0){
+showSlide();
+}
+
+/* DOTS */
+function currentSlide(n){
+index = n-1;
+showSlide();
+}
+
+
 
 
 
